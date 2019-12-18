2333d24ce05a2faf98a12894538e9cac15f6cec8
Michal <michal.nabie@gmail.com>
2019-12-18 19:30:37 +0100

    Added coin checker

---
+++ VendingMachine/Services/CoinService.cs
@@ -0,0 +1,20 @@
+﻿namespace VendingMachine.Services
+{
+    public class CoinService
+    {
+        
+        public bool CheckCoin(double coin)
+        {
+            return coin switch
+            {
+                0.01 => true,
+                0.05 => true,
+                0.10 => true,
+                0.25 => true,
+                0.50 => true,
+                1 => true,
+                _ => false
+            };
+        }
+    }
+}
---
+++ VendingMachineTests/CoinService.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace VendingMachineTests
+{
+    public class CoinService
+    {
+        [Test]
+        public void AcceptingOnlyCorrectCoins()
+        {
+            var coinService = new VendingMachine.Services.CoinService();
+            
+            Assert.AreEqual(true, coinService.CheckCoin(0.01));
+            Assert.AreEqual(true, coinService.CheckCoin(0.05));
+            Assert.AreEqual(true, coinService.CheckCoin(0.25));
+            Assert.AreEqual(true, coinService.CheckCoin(0.50));
+            Assert.AreEqual(true, coinService.CheckCoin(1));
+            Assert.AreEqual(false, coinService.CheckCoin(1.1));
+            Assert.AreEqual(false, coinService.CheckCoin(-1));
+        }
+    }
+}
