2e46696a6974e326d7f474b39fee1be98c69e592
Michal <michal.nabie@gmail.com>
2019-12-18 19:53:48 +0100

    Added coin enum model

---
+++ VendingMachine/Models/Coin.cs
@@ -0,0 +1,13 @@
+﻿namespace VendingMachine.Models
+{
+    public enum Coin
+    {
+        Cent = 1,
+        Nickel = 5,
+        Dime = 10,
+        QuarterDollar = 25,
+        HalfDollar = 50,
+        Dollar = 100,
+        BadCoin
+    }
+}
