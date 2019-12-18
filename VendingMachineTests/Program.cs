0bb921a23cb3c8455a66884cca0ae8a4d2711efb
Michal <michal.nabie@gmail.com>
2019-12-18 17:58:21 +0100

    init

---
+++ VendingMachineTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VendingMachineTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
