4615206def7e2235e45c094e26f41be268318f7c
Michal <michal.nabie@gmail.com>
2019-12-18 20:40:25 +0100

    Added Machine model

---
+++ VendingMachine/Models/Machine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachine.Models
+{
+    public class Machine
+    {
+        public double Wallet { get; set; }
+        public Dictionary<Product, int> Products { get; set; } 
+        public Dictionary<Coin, int> Coins { get; set; }
+        public Dictionary<double, int> RejectCoins { get; set; }
+
+        public Machine()
+        {
+            Products = new Dictionary<Product, int>();
+            Coins = new Dictionary<Coin, int>();
+            RejectCoins = new Dictionary<double, int>();
+        }
+    }
+}
