c65309e30dbcb9c553b542c30a3ab8b0594aa6f7
Michal Nabielec <michal.nabi@gmail.com>
2019-12-19 14:02:42 +0100

    Added machine controller

---
+++ VendingMachine/Controllers/MachineController.cs
@@ -0,0 +1,29 @@
+﻿using VendingMachine.Models;
+using VendingMachine.Services;
+
+namespace VendingMachine
+{
+    public class MachineController
+    {
+        private Machine Machine { get; set; }
+        private CoinService CoinService { get; set; }
+        public MachineServices MachineServices { get; set; }
+
+        public MachineController(Machine machine, CoinService coinService)
+        {
+            Machine = machine;
+            CoinService = coinService;
+            MachineServices = new MachineServices(machine,coinService);
+        }
+
+
+
+        public void BuyItem()
+        {
+            
+        }
+        
+        
+        
+    }
+}
