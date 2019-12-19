9d957679e50c26649512515f23587b2d64cd235e
Michal Nabielec <michal.nabi@gmail.com>
2019-12-19 13:10:50 +0100

    Added constructor to product

--- VendingMachine/Models/Product.cs
+++ VendingMachine/Models/Product.cs
@@ -5,5 +5,12 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public int Amount { get; set; }
+
+        public Product(string name, double price, int amount)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
     }
 }
