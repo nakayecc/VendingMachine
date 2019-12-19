9f6dbee05232b4196f969a72289e2c40f4da64dc
Michal Nabielec <michal.nabi@gmail.com>
2019-12-19 13:12:04 +0100

    Added Fetch product data

---
+++ VendingMachine/Dao/IProductDataFetch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using VendingMachine.Models;
+
+namespace VendingMachine.Dao
+{
+    public interface IProductDataFetch
+    {
+        List<Product> GetAllProduct();
+        Product GetProductByName(string name);
+    }
+}
---
+++ VendingMachine/Dao/ProductDataFetch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using VendingMachine.Models;
+
+namespace VendingMachine.Dao
+{
+    public class ProductDataFetch : IProductDataFetch
+    {
+        private readonly string _productPath;
+
+        public ProductDataFetch()
+        {
+            _productPath = JObject.Parse(File.ReadAllText(@"..\..\..\appsettings.json")).SelectToken("$..ProductPath")
+                .ToString();
+        }
+
+        public List<Product> GetAllProduct()
+        {
+            const int name = 0;
+            const int amount = 1;
+            const int price = 2;
+
+            var lines = File.ReadAllLines(_productPath).Select(a => a.Split(','));
+            return lines.Select(line => new Product(
+                    line[name].ToString(),
+                    Convert.ToDouble(line[price].Replace(".", ",")),
+                    Convert.ToInt32(line[amount])))
+                .ToList();
+        }
+
+        public Product GetProductByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
