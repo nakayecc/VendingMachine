7b901061bf593f714e15593ece7d15cc112d3856
Michal Nabielec <michal.nabi@gmail.com>
2019-12-19 10:06:18 +0100

    Added product model

---
+++ VendingMachine/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace VendingMachine.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
